25148c098a1f894f1ff48a5765f10b5cecb66f34
BiliksuunSamuel <samuel.biliksuun@hubtel.com>
2024-12-11 07:40:34 +0000

    updated app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "./views/ThemeProvider";
-import { Layout } from "./components/Layout";
-import { AcademicTracker } from "./pages/AcademicTracker";
-import { Admissions } from "./pages/Admissions";
-import { FeePayment } from "./pages/FeePayment";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<AcademicTracker />} />
-            <Route path="/admissions" element={<Admissions />} />
-            <Route path="/fee-payment" element={<FeePayment />} />
-          </Routes>
-        </Layout>
-      </Router>
+      <AppRouter />
     </ThemeProvider>
   );
 }
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,19 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { AcademicTracker } from "../pages/AcademicTracker";
+import { FeePayment } from "../pages/FeePayment";
+import { Admissions } from "../pages/Admissions";
+import { Layout } from "../components/Layout";
+
+export default function AppRouter() {
+  return (
+    <HashRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<AcademicTracker />} />
+          <Route path="/admissions" element={<Admissions />} />
+          <Route path="/fees-payment" element={<FeePayment />} />
+        </Routes>
+      </Layout>
+    </HashRouter>
+  );
+}
